78db9249eaaa3dfda6a130b5dc53074421560e1b
Franco <francorr240@gmail.com>
2023-08-22 15:49:09 -0700

    Task 0 complete

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,17 +3,17 @@
 import requests
 import sys
 
-
-user_id = sys.argv[1]
-res = f"todos?userId={user_id}&completed=true"
-user_info = requests.get(
-    f"https://jsonplaceholder.typicode.com/users/{user_id}")
-total_response = requests.get(
-    f"https://jsonplaceholder.typicode.com/todos?userId={user_id}")
-completed_res = requests.get(f"https://jsonplaceholder.typicode.com/{res}")
-user_name = user_info.json()["name"]
-completed = len(completed_res.json())
-total = len(total_response.json())
-print(f"Employee {user_name} is done with tasks({completed}/{total}):")
-for x in completed_res.json():
-    print(f"\t {x.get('title')}")
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    res = f"todos?userId={user_id}&completed=true"
+    user_info = requests.get(
+        f"https://jsonplaceholder.typicode.com/users/{user_id}")
+    total_response = requests.get(
+        f"https://jsonplaceholder.typicode.com/todos?userId={user_id}")
+    completed_res = requests.get(f"https://jsonplaceholder.typicode.com/{res}")
+    user_name = user_info.json()["name"]
+    completed = len(completed_res.json())
+    total = len(total_response.json())
+    print(f"Employee {user_name} is done with tasks({completed}/{total}):")
+    for x in completed_res.json():
+        print(f"\t {x.get('title')}")
